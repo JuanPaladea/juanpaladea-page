7b1d0caefce009d1cf6e00db1f2b7ab6c074cb4b
Juan Paladea <juanpaladea5@gmail.com>
2024-10-28 15:32:07 -0300

    Refactor HeroComponent and add export statement

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,23 @@
 import HeroComponent from './components/HeroComponent'
 import ProjectsComponent from './components/ProjectsComponent'
+import TechnologiesComponent from './components/TechnologiesComponent'
+import HeaderComponent from './components/HeaderComponent'
+import FooterComponent from './components/FooterComponent'
+import DividerComponent from './components/DividerComponent'
+import ContactForm from './components/ContactForm'
 
 function App() {
   return (
     <div>
+      <HeaderComponent />
       <HeroComponent />
+      <DividerComponent title="Technologies"/>
+      <TechnologiesComponent />
+      <DividerComponent title="Projects"/>
       <ProjectsComponent />
+      <DividerComponent title="Contact"/>
+      <ContactForm />
+      <FooterComponent />
     </div>
   )
 }
