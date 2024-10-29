b364d3f52367ab0be19e2cd5d6e7a0e0af3ab59f
Juan Paladea <juanpaladea5@gmail.com>
2024-10-29 15:55:55 -0300

    Refactor ProjectsComponent and TechnologiesComponent

--- src/components/ProjectsComponent.tsx
+++ src/components/ProjectsComponent.tsx
@@ -6,11 +6,37 @@ const ProjectsComponent = () => {
           <div className="flex flex-wrap -m-4">
             <div className="p-4 md:w-1/2 lg:w-1/3">
               <div className="h-full border-2 border-gray-800 rounded-lg overflow-hidden">
-                <img
-                  className="lg:h-48 md:h-36 w-full object-cover object-center"
-                  src="https://dummyimage.com/720x400"
-                  alt="blog"
-                />
+                <div className="relative group">
+                  <img
+                    className="lg:h-48 md:h-36 w-full object-cover object-center transition-transform duration-500 transform group-hover:rotate-y-180"
+                    src="https://dummyimage.com/720x400"
+                    alt="blog"
+                  />
+                  <div className="absolute inset-0 bg-gray-900 bg-opacity-75 flex items-center justify-center text-white opacity-0 group-hover:opacity-100 transition-opacity duration-500">
+                    <div className="h-full flex items-center p-4 rounded-lg hover:cursor-pointer">
+                      <img
+                        alt="team"
+                        className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                        src="https://dummyimage.com/88x88"
+                      />
+                      <img
+                        alt="team"
+                        className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                        src="https://dummyimage.com/88x88"
+                      />
+                      <img
+                        alt="team"
+                        className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                        src="https://dummyimage.com/88x88"
+                      />
+                      <img
+                        alt="team"
+                        className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                        src="https://dummyimage.com/88x88"
+                      />
+                    </div>
+                  </div>
+                </div>
                 <div className="p-6">
                   <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">
                     Front End
@@ -27,9 +53,9 @@ const ProjectsComponent = () => {
                       Live demo
                     </a>
                     <span className="text-gray-500 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1">
-                    <a className="text-indigo-400 inline-flex items-center md:mb-2 lg:mb-0">
-                      Github
-                    </a>
+                      <a className="text-indigo-400 inline-flex items-center md:mb-2 lg:mb-0">
+                        Github
+                      </a>
                     </span>
                   </div>
                 </div>
@@ -58,9 +84,9 @@ const ProjectsComponent = () => {
                       Live demo
                     </a>
                     <span className="text-gray-500 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1">
-                    <a className="text-indigo-400 inline-flex items-center md:mb-2 lg:mb-0">
-                      Github
-                    </a>
+                      <a className="text-indigo-400 inline-flex items-center md:mb-2 lg:mb-0">
+                        Github
+                      </a>
                     </span>
                   </div>
                 </div>
@@ -89,9 +115,9 @@ const ProjectsComponent = () => {
                       Live demo
                     </a>
                     <span className="text-gray-500 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1">
-                    <a className="text-indigo-400 inline-flex items-center md:mb-2 lg:mb-0">
-                      Github
-                    </a>
+                      <a className="text-indigo-400 inline-flex items-center md:mb-2 lg:mb-0">
+                        Github
+                      </a>
                     </span>
                   </div>
                 </div>
--- src/components/TechnologiesComponent.tsx
+++ src/components/TechnologiesComponent.tsx
@@ -1,75 +1,139 @@
 const TechnologiesComponent = () => {
+  const languages = {
+    html: {
+      name: "HTML",
+      icon: "https://dummyimage.com/80x80",
+    },
+    css: {
+      name: "CSS",
+      icon: "https://dummyimage.com/84x84",
+    },
+    js: {
+      name: "JavaScript",
+      icon: "https://dummyimage.com/88x88",
+    },
+    ts: {
+      name: "TypeScript",
+      icon: "https://dummyimage.com/88x88",
+    },
+    python: {
+      name: "Python",
+      icon: "https://dummyimage.com/88x88",
+    },
+    c: {
+      name: "C",
+      icon: "https://dummyimage.com/88x88",
+    },
+  }
+  
+  const frameWorks = {
+    node: {
+      name: "Node",
+      icon: "https://dummyimage.com/88x88",
+    },
+    express: {
+      name: "Express",
+      icon: "https://dummyimage.com/88x88",
+    },
+    flask: {
+      name: "Flask",
+      icon: "https://dummyimage.com/88x88",
+    },
+    reactVite: {
+      name: "React Vite",
+      icon: "https://dummyimage.com/88x88",
+    },
+  }
+
+  const dataBases = {
+    mongo: {
+      name: "MongoDB",
+      icon: "https://dummyimage.com/88x88",
+    },
+    sql: {
+      name: "SQL",
+      icon: "https://dummyimage.com/88x88",
+    },
+    firebase: {
+      name: "Firebase",
+      icon: "https://dummyimage.com/88x88",
+    },
+  }
+
   return (
     <>
       <section className="text-gray-400 bg-gray-900 body-font">
         <div className="container px-5 py-12 mx-auto">
           <h1 className="sm:w-2/5 text-white font-medium title-font text-xl mb-2">
-            Programming
+            Languages
           </h1>
           <div className="flex flex-wrap -m-2">
-            <div className="p-2 lg:w-1/4 md:w-1/2 w-full">
-              <div className="h-full flex items-center border-gray-800 border p-4 rounded-lg">
-                <img
-                  alt="team"
-                  className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
-                  src="https://dummyimage.com/80x80"
-                />
-                <div className="flex-grow">
-                  <h2 className="text-white title-font font-medium">
-                    Holden Caulfield
-                  </h2>
-                  <p className="text-gray-600">UI Designer</p>
+            {Object.values(languages).map((language) => (
+              <div className="p-2 lg:w-1/5 md:w-1/3 w-1/2 w-full">
+                <div className="h-full flex items-center border-gray-800 border p-4 rounded-lg">
+                  <img
+                    alt="team"
+                    className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                    src={language.icon}
+                  />
+                  <div className="flex-grow">
+                    <h2 className="text-white title-font font-medium">
+                      {language.name}
+                    </h2>
+                  </div>
                 </div>
               </div>
-            </div>
-            <div className="p-2 lg:w-1/4 md:w-1/2 w-full">
-              <div className="h-full flex items-center border-gray-800 border p-4 rounded-lg">
-                <img
-                  alt="team"
-                  className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
-                  src="https://dummyimage.com/84x84"
-                />
-                <div className="flex-grow">
-                  <h2 className="text-white title-font font-medium">
-                    Henry Letham
-                  </h2>
-                  <p className="text-gray-600">CTO</p>
-                </div>
-              </div>
-            </div>
-            <div className="p-2 lg:w-1/4 md:w-1/2 w-full">
-              <div className="h-full flex items-center border-gray-800 border p-4 rounded-lg">
-                <img
-                  alt="team"
-                  className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
-                  src="https://dummyimage.com/88x88"
-                />
-                <div className="flex-grow">
-                  <h2 className="text-white title-font font-medium">
-                    Oskar Blinde
-                  </h2>
-                  <p className="text-gray-600">Founder</p>
-                </div>
-              </div>
-            </div>
-            <div className="p-2 lg:w-1/4 md:w-1/2 w-full">
-              <div className="h-full flex items-center border-gray-800 border p-4 rounded-lg">
-                <img
-                  alt="team"
-                  className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
-                  src="https://dummyimage.com/88x88"
-                />
-                <div className="flex-grow">
-                  <h2 className="text-white title-font font-medium">
-                    Oskar Blinde
-                  </h2>
-                  <p className="text-gray-600">Founder</p>
-                </div>
-              </div>
-            </div>
+            ))}
           </div>
         </div>
 
+        <div className="container px-5 py-12 mx-auto">
+          <h1 className="sm:w-2/5 text-white font-medium title-font text-xl mb-2">
+            FrameWorks
+          </h1>
+          <div className="flex flex-wrap -m-2">
+            {Object.values(frameWorks).map((framework) => (
+              <div className="p-2 lg:w-1/5 md:w-1/3 w-1/2 w-full ">
+                <div className="h-full flex items-center border-gray-800 border p-4 rounded-lg">
+                  <img
+                    alt="team"
+                    className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                    src={framework.icon}
+                  />
+                  <div className="flex-grow">
+                    <h2 className="text-white title-font font-medium">
+                      {framework.name}
+                    </h2>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+
+        <div className="container px-5 py-12 mx-auto">
+          <h1 className="sm:w-2/5 text-white font-medium title-font text-xl mb-2">
+            Databases
+          </h1>
+          <div className="flex flex-wrap -m-2">
+            {Object.values(dataBases).map((database) => (
+              <div className="p-2 lg:w-1/5 md:w-1/3 w-1/2 w-full ">
+                <div className="h-full flex items-center border-gray-800 border p-4 rounded-lg">
+                  <img
+                    alt="team"
+                    className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
+                    src={database.icon}
+                  />
+                  <div className="flex-grow">
+                    <h2 className="text-white title-font font-medium">
+                      {database.name}
+                    </h2>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
         
       </section>
     </>
