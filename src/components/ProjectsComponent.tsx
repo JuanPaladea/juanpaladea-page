4d66ad53fab9ffad02ea3ba5c73d00c87e5ae374
Juan Paladea <juanpaladea5@gmail.com>
2024-10-31 15:04:45 -0300

    Refactor ProjectsComponent with updated images, links, and descriptions for backend and e-commerce projects

--- src/components/ProjectsComponent.tsx
+++ src/components/ProjectsComponent.tsx
@@ -73,11 +73,11 @@ const ProjectsComponent = () => {
                 <div className="relative group">
                   <img
                     className="lg:h-48 md:h-36 w-full object-cover object-center transition-transform duration-500 transform group-hover:rotate-y-180"
-                    src="https://dummyimage.com/720x400"
-                    alt="blog"
+                    src="https://i.imgur.com/w8aEHii.png"
+                    alt="Backend e-commerce"
                   />
                   <div className="absolute inset-0 bg-gray-900 bg-opacity-75 flex items-center justify-center text-white opacity-0 group-hover:opacity-100 transition-opacity duration-500">
-                    <div className="h-full flex items-center p-4 rounded-lg hover:cursor-pointer">
+                    <a href="https://backend-practice-ml8a.onrender.com/api-docs/" target="_blank" className="h-full flex items-center p-4 rounded-lg hover:cursor-pointer">
                       <img
                         alt="team"
                         className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
@@ -98,26 +98,25 @@ const ProjectsComponent = () => {
                         className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
                         src="https://dummyimage.com/88x88"
                       />
-                    </div>
+                    </a>
                   </div>
                 </div>
                 <div className="p-6">
                   <h2 className="tracking-widest text-xs title-font font-medium text-gray-500 mb-1">
-                    Front End
+                    Back End
                   </h2>
                   <h1 className="title-font text-lg font-medium text-white mb-3">
-                    Libelulis
+                    E-Commerce Backend
                   </h1>
                   <p className="leading-relaxed mb-3">
-                    Photo booth fam kinfolk cold-pressed sriracha leggings
-                    jianbing microdosing tousled waistcoat.
+                    Backend application that uses Node.js, Express, and MongoDB. The application provides a platform for managing products, shopping carts, messages, and tickets. It also includes user authentication and authorization.
                   </p>
                   <div className="flex items-center flex-wrap ">
-                    <a className="text-indigo-400 inline-flex items-center md:mb-2 lg:mb-0">
+                    <a href="https://backend-practice-ml8a.onrender.com/api-docs/" target="_blank" className="text-indigo-400 inline-flex items-center md:mb-2 lg:mb-0">
                       Live demo
                     </a>
                     <span className="text-gray-500 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1">
-                      <a className="text-indigo-400 inline-flex items-center md:mb-2 lg:mb-0">
+                      <a href="https://github.com/JuanPaladea/Backend-Practice" target="_blank" className="text-indigo-400 inline-flex items-center md:mb-2 lg:mb-0">
                         Github
                       </a>
                     </span>
@@ -132,11 +131,11 @@ const ProjectsComponent = () => {
                 <div className="relative group">
                   <img
                     className="lg:h-48 md:h-36 w-full object-cover object-center transition-transform duration-500 transform group-hover:rotate-y-180"
-                    src="https://dummyimage.com/720x400"
+                    src="https://i.imgur.com/PeWFKkS.png"
                     alt="blog"
                   />
                   <div className="absolute inset-0 bg-gray-900 bg-opacity-75 flex items-center justify-center text-white opacity-0 group-hover:opacity-100 transition-opacity duration-500">
-                    <div className="h-full flex items-center p-4 rounded-lg hover:cursor-pointer">
+                    <a href="https://libelulis.vercel.app/" target="_blank" className="h-full flex items-center p-4 rounded-lg hover:cursor-pointer">
                       <img
                         alt="team"
                         className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
@@ -157,7 +156,7 @@ const ProjectsComponent = () => {
                         className="w-16 h-16 bg-gray-100 object-cover object-center flex-shrink-0 rounded-full mr-4"
                         src="https://dummyimage.com/88x88"
                       />
-                    </div>
+                    </a>
                   </div>
                 </div>
                 <div className="p-6">
@@ -168,15 +167,14 @@ const ProjectsComponent = () => {
                     Libelulis
                   </h1>
                   <p className="leading-relaxed mb-3">
-                    Photo booth fam kinfolk cold-pressed sriracha leggings
-                    jianbing microdosing tousled waistcoat.
+                    An e-commerce platform offering various features such as user authentication, purchase history, contact forms, newsletter subscriptions, and admin functionalities.
                   </p>
                   <div className="flex items-center flex-wrap ">
-                    <a className="text-indigo-400 inline-flex items-center md:mb-2 lg:mb-0">
+                    <a href="https://libelulis.vercel.app/" target="_blank" className="text-indigo-400 inline-flex items-center md:mb-2 lg:mb-0">
                       Live demo
                     </a>
                     <span className="text-gray-500 mr-3 inline-flex items-center lg:ml-auto md:ml-0 ml-auto leading-none text-sm pr-3 py-1">
-                      <a className="text-indigo-400 inline-flex items-center md:mb-2 lg:mb-0">
+                      <a href="https://github.com/JuanPaladea/Libelulis" target="_blank" className="text-indigo-400 inline-flex items-center md:mb-2 lg:mb-0">
                         Github
                       </a>
                     </span>
